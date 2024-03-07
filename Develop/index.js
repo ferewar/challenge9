81b917c30f1ee422cad51d06468feccafa4bbdce
ferewar <73423237+ferewar@users.noreply.github.com>
2024-03-06 23:24:03 -0800

    first commit

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,77 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a short description of your project:',
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps required to install your project?',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'How do you use this project?',
+      },
+      {
+        type: 'input',
+        name: 'contributing',
+        message: 'How can others contribute to this project?',
+      },
+      {
+        type: 'input',
+        name: 'tests',
+        message: 'What commands should be run to run tests?',
+      },
+      {
+        type: 'input',
+        name: 'questions',
+        message: 'Enter your GitHub username:',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address:',
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: 'What kind of license should your project have?',
+        choices: ['MIT', 'GPL-3.0', 'Apache-2.0', 'None'],
+      },
+    
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        console.log('README.md has been generated successfully!');
+      });
+    
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        const readmeContent = generateMarkdown(answers);
+        writeToFile('README.md', readmeContent);
+      });
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,100 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
+function renderLicenseBadge(license) {
+  switch(license) {
+    case 'MIT':
+      return '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)';
+    case 'GPL-3.0':
+      return '![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)';
+    case 'Apache-2.0':
+      return '![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)';
+    default:
+      return '';
+  }
+}
 function renderLicenseBadge(license) {}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
+function renderLicenseLink(license) {
+  switch(license) {
+    case 'MIT':
+      return 'https://opensource.org/licenses/MIT';
+    case 'GPL-3.0':
+      return 'https://www.gnu.org/licenses/gpl-3.0';
+    case 'Apache-2.0':
+      return 'https://opensource.org/licenses/Apache-2.0';
+    default:
+      return '';
+  }
+}
 function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
+function renderLicenseSection(license) {
+  if (license === '' || license === 'None') {
+    return '';
+  } else {
+    return `## License
+
+This project is licensed under the ${license} License - see the [LICENSE](${renderLicenseLink(license)}) file for details.`;
+  }
+}
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  const githubURL = data.questions ? `https://github.com/${data.questions}` : '';
+  const emailContact = data.email ? data.email : 'No email provided';
 
-`;
-}
+  return `# ${data.title}
+  ${renderLicenseBadge(data.license)}
+  
+  ## Description
+  
+  ${data.description}
+  
+  ${renderLicenseSection(data.license)}
+  
+  ## Table of Contents
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  
+  To install necessary dependencies, run the following command:
+  
+  \`\`\`
+  ${data.installation}
+  \`\`\`
+  
+  ## Usage
+  
+  ${data.usage}
+  
+  ## Contributing
+  
+  ${data.contributing}
+  
+  ## Tests
+  
+  To run tests, run the following command:
+  
+  \`\`\`
+  ${data.tests}
+  \`\`\`
+  
+  ## Questions
+  
+  If you have any questions about the repo, open an issue or contact me directly at ${emailContact}. You can find more of my work at [${data.questions}](${githubURL}).
+  
+  `;
+  }
+  
 
 module.exports = generateMarkdown;
